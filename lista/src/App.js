b39f812a492b2f5c5ee88d0e3c5c4746b588e843
Felipe Butzke <felip.ab@gmail.com>
2020-12-03 20:14:11 -0300

    importado css, adicionado form.js

---
+++ lista/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Minha lista de compras</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
