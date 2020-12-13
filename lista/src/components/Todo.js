917d9c4a46212e76ddb6adad26a37c96f9439c0b
Felipe Butzke <felip.ab@gmail.com>
2020-12-13 09:00:00 -0300

    Arrumando botões, clicando no adicionar, o form updates sem nada= value={inputText} setInputText("");

--- lista/src/components/Todo.js
+++ lista/src/components/Todo.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const Todo = ({text}) => {
+const Todo = ({text, todo, todos, setTodos }) => {
+  //events
+  const deleteHandler = () => {
+  setTodos(todos.filter((el) => el.id !== todos.id));
+  };
     return(
         <div className="todo">
       <li className="todo-item">{text}</li>
       <button className="complete-btn"><i className="fas fa-check"></i></button>
-      <button className="trash-btn"><i className="fas fa-trash"></i></button>
+      <button onclick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
 
     </div>
 
